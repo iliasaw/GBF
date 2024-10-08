35bcc789519bab02d0d9fced332c0b8a2fcb2e05
iliasaw <62304861+iliasaw@users.noreply.github.com>
2024-10-09 00:16:17 +0300

    Update warp_generator.sh

--- warp_generator.sh
+++ warp_generator.sh
@@ -47,27 +47,27 @@ generate_config() {
     port=$(echo "$peer_endpoint" | sed 's/.*:\([0-9]*\)$/\1/')
     peer_endpoint=$(echo "$peer_endpoint" | sed 's/\(.*\):[0-9]*/162.159.193.5/')
 
-    conf=$(cat <<-EOM
-    [Interface]
-    PrivateKey = ${priv}
-    S1 = 0
-    S2 = 0
-    Jc = 120
-    Jmin = 23
-    Jmax = 911
-    H1 = 1
-    H2 = 2
-    H3 = 3
-    H4 = 4
-    Address = ${client_ipv4}, ${client_ipv6}
-    DNS = 1.1.1.1, 2606:4700:4700::1111, 1.0.0.1, 2606:4700:4700::1001
+    conf=$(cat <<EOM
+[Interface]
+PrivateKey = ${priv}
+S1 = 0
+S2 = 0
+Jc = 120
+Jmin = 23
+Jmax = 911
+H1 = 1
+H2 = 2
+H3 = 3
+H4 = 4
+Address = ${client_ipv4}, ${client_ipv6}
+DNS = 1.1.1.1, 2606:4700:4700::1111, 1.0.0.1, 2606:4700:4700::1001
 
-    [Peer]
-    PublicKey = ${peer_pub}
-    AllowedIPs = 0.0.0.0/0, ::/0
-    Endpoint = ${peer_endpoint}:${port}
-    EOM
-    )
+[Peer]
+PublicKey = ${peer_pub}
+AllowedIPs = 0.0.0.0/0, ::/0
+Endpoint = ${peer_endpoint}:${port}
+EOM
+)
 
     echo -e "\n########## КОНФИГ ${index} ##########"
     echo "${conf}"
